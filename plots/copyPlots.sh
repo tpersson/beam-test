ad5f4b85aaeffaa1ad9ae79fab9cddb07bde75ac
Tobias Hakan Bjorn Persson <tpersson@lxplus776.cern.ch>
2022-12-01 17:12:41 +0100

    More plots added

---
+++ plots/copyPlots.sh
@@ -0,0 +1,6 @@
+cp ../../../COM_2021_10_20_injection_swapped_q7/Beam2/beta_beat_before_after_pre_beam1.pdf beam1
+cp ../../../COM_2021_10_20_injection_swapped_q7/Beam2/beta_beat_before_after_pre_beam2.pdf beam2
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2021/COM_2021_10_22_injection_correction/Beam1/*.pdf beam1
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2021/COM_2021_10_22_injection_correction/Beam2/*.pdf beam2
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_04_23_injection_validation/plots/lhcb1*.pdf beam1
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_04_23_injection_validation/plots/lhcb2*.pdf beam2
