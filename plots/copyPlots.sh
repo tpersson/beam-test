f5c2399c80332898b5751fdb0b0ceae5f3e8aa8e
Tobias Hakan Bjorn Persson <tpersson@lxplus776.cern.ch>
2022-12-01 19:49:55 +0100

    More plots added..

--- plots/copyPlots.sh
+++ plots/copyPlots.sh
@@ -4,3 +4,4 @@ cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2021/COM_2021_10_22_injection_c
 cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2021/COM_2021_10_22_injection_correction/Beam2/*.pdf beam2
 cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_04_23_injection_validation/plots/lhcb1*.pdf beam1
 cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_04_23_injection_validation/plots/lhcb2*.pdf beam2
+cp /user/slops/data/LHC_DATA/OP_DATA/Betabeat/2022-07-22/LHCB2/Results/first_day_3qy_ramp.pdf beam2/comp_injection_several.pdf
